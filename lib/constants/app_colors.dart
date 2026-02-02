73ba4d589b93e6356744906f14aeda9fcee08857
Randomlyclueless <144950366+Randomlyclueless@users.noreply.github.com>
2026-02-02 12:11:19 +0530

    set up docker file and created first landing page in flutter

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF5B8DEF);
+  static const Color secondary = Color(0xFF7ED6A7);
+
+  static const Color background = Color(0xFFF8F9FB);
+
+  static const Color textPrimary = Color(0xFF1F2933);
+  static const Color textSecondary = Color(0xFF6B7280);
+
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+}
---
+++ lib/widgets/app_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import '../constants/app_text_styles.dart';
+
+class AppButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const AppButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Text(
+        text,
+        style: AppTextStyles.buttonText,
+      ),
+    );
+  }
+}
