17d409e0c196716cf6ec64068fa80cc14087ef08
Hitu Bharal <hitu.bharal@gmail.com>
2026-02-21 17:41:59 +0530

    deleting files
    deleting redundant files

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -11,7 +11,5 @@ def create_app():
     from .routes import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    from .routes.consultant import consultant as consultant_blueprint
-    app.register_blueprint(consultant_blueprint)
-
     return app
+
